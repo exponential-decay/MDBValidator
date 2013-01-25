7c76541fd09656a9dd1d0addc777d857713a1b9e
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2013-01-25 14:48:54 +0000

    [documentation] Code comments.

--- MDBMarkers.py
+++ MDBMarkers.py
@@ -2,10 +2,10 @@ class MDBMarkers:
 	
 	MDB_BOF = 0x00
 	MDB_VERSION_OFFSET = 0x14
-	MDB_VERSION_HEADER_LEN = 0x15 # 0x14 + 0x01
 	
-	DEFINITION_OFFSET = 128
+	MDB_DEFINITION_LEN = 0x255	# length which contains the data we need
 	
+	# Internal byte values
 	JET3 = 0x00	# Access 97
 	JET4 = 0x01	# Access 2000/2002
 	JET5 = 0x02
@@ -15,13 +15,16 @@ class MDBMarkers:
 	
 	JETVER = {JET3 : "Jet 3, Access 97", JET4 : "Jet 4, Access 2000/2002", JET5 : "Jet 5, Unknown", JETX : "Jet Unknown, Access 2010"}
 	
+	# Page sizes
 	JET3PAGESIZE = 2048
 	JET4PAGESIZE = 4096
 	PAGESIZEZERO = 0
 	
+	# Magic number offset and length
 	MAGIC_OFF = 0x00
 	MAGIC_LEN = 4
 	
+	# Format ID offset and length
 	FORMAT_ID_OFF = 0x04	
 	FORMAT_ID_LEN = 16
 	

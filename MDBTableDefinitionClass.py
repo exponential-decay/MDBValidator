34e454c0b7d629d84b62583fa16b8a96c88e58e9
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2013-01-31 02:33:55 +0000

    [feature] Filling out table definition class.

--- MDBTableDefinitionClass.py
+++ MDBTableDefinitionClass.py
@@ -1,7 +1,27 @@
+import binascii
+import struct
+
 class MDBTableDefinitionValidator:
 
+	IDOFF = 0x02
+	IDLEN = 0x02
+
+	tdef_id = ''
+
+	DEFBLOCKOFF = 0x08
+	DEFBLOCKLEN97 = 0x23
+	DEFBLOCKLEN2K = 0x37
+
 	def __init__(self):
-		print "table definition class"
+		c = 'do nothing'
 		
 	def handleMDBTableDefinition(self, buf):
-		print "handle table definition"
+	
+		self.tdef_id = binascii.hexlify(buf[self.IDOFF : self.IDOFF + self.IDLEN])		
+		tddefblock = buf[self.DEFBLOCKOFF : self.DEFBLOCKOFF + self.DEFBLOCKLEN97]
+		tdtype = binascii.hexlify(tddefblock[0xC])
+		
+	def __write2file__(self, name, buf):
+		f = open(name, 'w+b')
+		f.write(buf)
+		f.close()
